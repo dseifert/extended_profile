29390c6f4c238b674562f0a750451260dac4e4ad
Daniel Seifert <dseifert@inf.fu-berlin.de>
2011-10-21 00:18:34 +0200

    chain changes to :account - fixes http://projects.andriylesyuk.com/issues/1850

--- lib/my_extended_controller_patch.rb
+++ lib/my_extended_controller_patch.rb
@@ -7,7 +7,7 @@ module MyExtendedControllerPatch
         base.send(:include, InstanceMethods)
         base.class_eval do
             unloadable
-            alias_method :account, :extended_account
+            alias_method_chain :account, :extended
         end
     end
 
@@ -35,37 +35,18 @@ module MyExtendedControllerPatch
         #    end
         #end
 
-        def extended_account
-            @user = User.current
-            @pref = @user.pref
-            @profile = @user.profile
+        def account_with_extended
             if request.post?
-                if @user.respond_to?('safe_attributes=')
-                    @user.safe_attributes = params[:user]
-                else # Redmine 1.0.x
-                    @user.attributes = params[:user]
-                    @user.mail_notification = (params[:notification_option] == 'all')
-                end
-                @user.pref.attributes = params[:pref]
+                @user = User.current
+                @pref = @user.pref
+                @profile = @user.profile
                 @user.profile.attributes = params[:profile]
-                @user.pref[:no_self_notified] = (params[:no_self_notified] == '1')
+
                 if (@user.valid? & @user.valid_profile?) && @user.save
-                    @user.pref.save
                     @user.profile.save
-                    if params[:notification_option] # Redmine 1.0.x
-                        @user.notified_project_ids = (params[:notification_option] == 'selected' ? params[:notified_project_ids] : [])
-                    else
-                        @user.notified_project_ids = (@user.mail_notification == 'selected' ? params[:notified_project_ids] : [])
-                    end
-                    set_language_if_valid(@user.language)
-                    flash[:notice] = l(:notice_account_updated)
                 end
             end
-            if User.columns_hash['mail_notification'].type == :boolean # Redmine 1.0.x
-                @notification_options = [ [ l(:label_user_mail_option_all), 'all' ], [ l(:label_user_mail_option_none), 'none' ] ]
-                @notification_options.insert(1, [ l(:label_user_mail_option_selected), 'selected' ]) if @user.memberships.length > 1
-                @notification_option = @user.mail_notification? ? 'all' : (@user.notified_projects_ids.empty? ? 'none' : 'selected')
-            end
+            account_without_extended
         end
 
     end
